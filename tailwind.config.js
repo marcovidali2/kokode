fc54d498b04a9f44fd543b722369dade69bc6008
marcovidali2 <marcovidali6@proton.me>
2024-06-30 09:27:44 +0200

    feat(app): Add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#e11d48",
+                    secondary: "#fb6000",
+                    accent: "#00b574",
+                    neutral: "#110a00",
+                    "base-100": "#FFFFFF",
+                    info: "#00bde4",
+                    success: "#aff772",
+                    warning: "#e56d00",
+                    error: "#ff617d",
+                },
+            },
+        ],
+    },
+    plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
+};
